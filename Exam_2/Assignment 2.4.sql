45d7559a125917ff4a32779665f536e69c1e6606
ER De Guzman <EmmanuelDG@magenic.com>
2021-09-29 20:27:32 +0800

    SQL Masters | Assignment 2.4 Push Modifications

--- Exam_2/Assignment 2.4.sql
+++ Exam_2/Assignment 2.4.sql
@@ -2,31 +2,36 @@ USE SQL4DevsDb
 GO
 -- 4. Top selling product per month year
 ;WITH cte_orders AS (
-	SELECT 
-		DATEPART(YEAR, o.OrderDate) AS OrderYear,
+	SELECT
+		DATEPART(YEAR, o.OrderDate)	 AS OrderYear,
 		DATEPART(MONTH, o.OrderDate) AS OrderMonth,
+		DATENAME(MONTH, o.OrderDate) AS OrderMonthName,
 		p.ProductName,
-		SUM(oi.Quantity) AS TotalQuantity
+		SUM(oi.Quantity)			 AS TotalQuantity
 	FROM dbo.[Order] o
 	INNER JOIN dbo.OrderItem oi ON oi.OrderId = o.OrderId
-	INNER JOIN dbo.Product p ON oi.ProductId = p.ProductId
-	INNER JOIN dbo.Store s ON o.StoreId = s.StoreId
-	GROUP BY o.OrderDate, p.ProductName
+	INNER JOIN dbo.Product p	ON p.ProductId = oi.ProductId
+	GROUP BY
+		p.ProductName,
+		DATEPART(YEAR, o.OrderDate),
+		DATEPART(MONTH, o.OrderDate),
+		DATENAME(MONTH, o.OrderDate)
 ),
-cte_top_selling_product AS (
+cte_selling_product_ranks AS (
 	SELECT
-		RANK() OVER (PARTITION BY co.OrderYear, co.OrderMonth ORDER BY co.TotalQuantity DESC) AS [RankID],
+		RANK() OVER (PARTITION BY co.OrderYear, co.OrderMonth ORDER BY co.TotalQuantity DESC) AS RankID,
 		co.OrderYear,
 		co.OrderMonth,
+		co.OrderMonthName,
 		co.ProductName,
 		co.TotalQuantity
-	FROM cte_orders co
+	FROM cte_orders co	
 )
 
-SELECT tsp.OrderYear,
-	DATENAME(MONTH, DATEADD(MONTH, tsp.OrderMonth, -1)) AS OrderMonth,
-	tsp.ProductName,
-	tsp.TotalQuantity
-FROM cte_top_selling_product tsp
-WHERE tsp.RankID = 1
-ORDER BY tsp.OrderYear,	tsp.OrderMonth ASC
+SELECT spr.OrderYear,
+	spr.OrderMonthName AS OrderMonth,
+	spr.ProductName,
+	spr.TotalQuantity
+FROM cte_selling_product_ranks spr
+WHERE spr.RankID = 1
+ORDER BY spr.OrderYear,	spr.OrderMonth ASC
