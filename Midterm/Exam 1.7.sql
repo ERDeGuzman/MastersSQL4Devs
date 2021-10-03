57d7e8cd7093906b7161b07a9014eaaa11469924
ER De Guzman <EmmanuelDG@magenic.com>
2021-10-04 06:45:58 +0800

    SQL Masters | Midterms 1.7

---
+++ Midterm/Exam 1.7.sql
@@ -0,0 +1,36 @@
+USE SQL4DevsDb
+GO
+
+DECLARE @columns NVARCHAR(MAX) = '', 
+		@sql     NVARCHAR(MAX) = '';
+
+;WITH order_months AS (
+	SELECT DISTINCT CAST(DATENAME(MONTH, o.OrderDate) AS CHAR(3)) AS [MonthName],
+		DATEPART(MONTH, o.OrderDate) AS [MonthNumber]
+	FROM dbo.[Order] o
+	INNER JOIN dbo.OrderItem oi ON oi.OrderID = o.OrderID 
+)
+
+SELECT @columns += QUOTENAME([MonthName]) + ','
+FROM order_months
+ORDER BY [MonthNumber]
+
+SET @columns = LEFT(@columns, LEN(@columns) - 1);
+
+SET @sql = '
+		WITH ProductsByCategoryCTE AS (
+			SELECT CAST(DATENAME(Month, o.OrderDate) AS CHAR(3)) AS Month,
+				ISNULL(oi.ListPrice, 0.00) AS ListPrice,
+				DATENAME(Year, o.OrderDate) AS SalesYear
+			FROM dbo.[Order] o
+			INNER JOIN dbo.OrderItem oi ON oi.OrderID = o.OrderID 	
+		)
+
+		SELECT * FROM ProductsByCategoryCTE 
+		PIVOT(
+			SUM(ListPrice)
+			FOR Month IN ('+ @columns +')
+		) AS PivotTable
+		ORDER BY SalesYear ASC'
+
+EXECUTE sp_executesql @sql
